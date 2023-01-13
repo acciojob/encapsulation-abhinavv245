6f19fab0c8f20aa48919126a3f99475b3806241e
Abhinav <abhinavgoyal245@gmail.com>
2023-01-13 12:26:41 +0530

    encapsulated class successfully

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+
+public static void main(String[] args) {
+    RWOnly obj= new RWOnly();
+//       obj.name= "Abhinav";
+//    System.out.println(obj.name);
+    //name has private access in com.driver.RWOnly
+    obj.setName("Abhinav");
+    System.out.println(obj.getName());
+}
 }
